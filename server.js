10266c5d89356b182beb3cb76767b154d1099af1
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-25 03:16:54 +0530

    Added a custom middleware for upper-casing the name

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const morgan = require('morgan');
 const userRouter = require('./user-router');
 const postRouter = require('./post-router');
 
-
-
 const server = express();
 
+
+function caseConverter(req, res, next) {
+    
+    if(req.body.name) {
+        const name = req.body.name
+        req.body.name = name.toUpperCase()
+        next()
+    } else {
+        next()
+    }
+}
 
 server.use(express.json());
 server.use(morgan('dev'));
@@ -18,7 +27,7 @@ server.get('/', (req, res, next) => {
     res.send('its working');
 })
 
-server.use('/api/users', userRouter);
+server.use('/api/users', caseConverter, userRouter);
 server.use('/api/posts', postRouter);
 
 
